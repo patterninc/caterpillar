98cc8a5108cb55a9f30d6b30a42d7652794681a9
Divyanshu Tiwari <33171967+divyanshu-tiwari@users.noreply.github.com>
2026-03-26 11:36:23 +0530

    use Adaptive mode retry to handle SSM throttling. (#50)
    * retry SSM:GetParameter after random delay to handle throttling
    
    * use adaptive mode retryer with increase max retry count

--- internal/pkg/config/secret.go
+++ internal/pkg/config/secret.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/retry"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
@@ -15,15 +16,25 @@ var (
 )
 
 func getSecret(path string) (string, error) {
+	var value *ssm.GetParameterOutput
+	var err error
 
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		return ``, err
 	}
 
-	svc := ssm.NewFromConfig(cfg)
+	svc := ssm.NewFromConfig(cfg, func(o *ssm.Options) {
+		o.Retryer = retry.NewAdaptiveMode(func(amo *retry.AdaptiveModeOptions) {
+			amo.StandardOptions = []func(*retry.StandardOptions){
+				func(so *retry.StandardOptions) {
+					so.MaxAttempts = 15
+				},
+			}
+		})
+	})
 
-	value, err := svc.GetParameter(ctx, &ssm.GetParameterInput{
+	value, err = svc.GetParameter(ctx, &ssm.GetParameterInput{
 		Name:           aws.String(path),
 		WithDecryption: awsTrue,
 	})
